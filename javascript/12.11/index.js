00d26502ee34f18b53260d296edbf3a1160a0f31
[firstname lastname] <[valid-email]>
2025-11-14 09:37:37 +0100

    h

---
+++ javascript/12.11/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Wolrd");
+console.error("Hello World");
+console.warn("Hello World");
+print("Hello World");
+
+for(let i = 0; i <10000 i++) {
+
+}
+console.timeEnd();
+console.table(["hodonota1", "hodnota2"]);
+let input1 = document.getElementById("1").value
+let input2 = document.getElementById("2").value
