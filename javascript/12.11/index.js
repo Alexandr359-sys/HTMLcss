fef2591238ec288f1639e259988bfd6203ac4d4e
[firstname lastname] <[valid-email]>
2025-11-14 09:40:29 +0100

    h

--- javascript/12.11/index.js
+++ javascript/12.11/index.js
@@ -8,5 +8,10 @@ for(let i = 0; i <10000 i++) {
 }
 console.timeEnd();
 console.table(["hodonota1", "hodnota2"]);
-let input1 = document.getElementById("1").value
-let input2 = document.getElementById("2").value
+
+function calc() {
+    
+    let input1 = document.getElementById("1").value 
+    let input2 = document.getElementById("2").value
+    console.log("1234");
+}
