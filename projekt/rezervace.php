1ea84a56379f91822ec5241ad9a36ca3a9ebd2ea
Alexandr359-sys <lelekalexandr@gmail.com>
2026-05-21 22:00:49 +0200

    hosting

--- projekt/rezervace.php
+++ projekt/rezervace.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "kino_db";
+$host = "sql301.infinityfree.com";
+$user = "if0_41961871";
+$pass = "Tonymatoni420";
+$dbname = "if0_41961871_kino_db";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
